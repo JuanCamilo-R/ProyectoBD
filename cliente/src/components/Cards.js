4053ea97b287ff9c2319aea9f78ef23765ce8016
JuanCamilo-R <51805884+JuanCamilo-R@users.noreply.github.com>
2021-04-16 12:42:50 -0500

    Acomodar form

--- cliente/src/components/Cards.js
+++ cliente/src/components/Cards.js
@@ -1,68 +1,18 @@
 import React from 'react';
-import ReactCardCarousel from 'react-card-carousel';
+import { Carousel } from 'react-bootstrap';
 import { useState } from 'react';
-import Slider from 'react-slick';
+import CarouselContainer from './CarouselContainer';
+
 class Card extends React.Component{
-    static get CONTAINER_STYLE(){
-        return {
-            position: "relative",
-            height: "100vh",
-            width: "100%",
-            display: "flex",
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "middle"
-        };
-     }
-    
-     static get CARD_STYLE(){
-         return {
-             height: "750px",
-             width: "750px",
-             paddingTop: "80px",
-             textAlign: "center",
-             background: "#52C0F5",
-             color: "#FFF",
-             frontFamily: "sans-serif",
-             fontSize: "12px",
-             textTransform: "uppercase",
-             borderRadius: "10px",
-             boxSizing: "border-box"
-         };
-     }
-    
-     render(){
-        const settings = {
-         infinite: true,
-         lazyLoad: true,
-         speed: 300,
-         slidesToShow: 3,
-         centerMode: true,
-         centerPadding: 0,
-        };
-         return(
-             <div className = "App" style = {Card.CONTAINER_STYLE}>
-                 <ReactCardCarousel autoplay = {false}>
-                    <div style = {Card.CARD_STYLE} onKeyDown>FIRST QUERY</div>
-                    <div style = {Card.CARD_STYLE}>SECOND QUERY</div>
-                    <div style = {Card.CARD_STYLE}>THIRD QUERY</div>
-                    <div style = {Card.CARD_STYLE}>FORTH QUERY</div>
-                    <div style = {Card.CARD_STYLE}>FITH QUERY</div>
-                </ReactCardCarousel>
-             </div>
-             /*
-            <div style = {Card.CONTAINER_STYLE}>
-            <ReactCardCarousel autoplay = {false}>
-                <div style = {Card.CARD_STYLE} onKeyDown>FIRST QUERY</div>
-                <div style = {Card.CARD_STYLE}>SECOND QUERY</div>
-                <div style = {Card.CARD_STYLE}>THIRD QUERY</div>
-                <div style = {Card.CARD_STYLE}>FORTH QUERY</div>
-                <div style = {Card.CARD_STYLE}>FITH QUERY</div>
-            </ReactCardCarousel>
-        </div>
-        */
-         )
-     }
+      render(){
+          return(
+              <div className = "content">
+                   <CarouselContainer/>
+              </div>
+           
+          );
+      }
+
 }
 
 export default Card;
---
+++ cliente/src/components/CarouselContainer.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import Form  from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { render } from 'react-dom';
+import logo from '../images/logo_prueba.png';
+
+function ControlledCarousel() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+          <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+            <Carousel.Caption>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <h1>HOLA</h1>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      );
+    }
+export default ControlledCarousel;
