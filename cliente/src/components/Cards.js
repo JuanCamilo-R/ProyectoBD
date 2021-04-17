a3176da2354a56ec2f7da5c9337a4177438c2788
JuanCamilo-R <51805884+JuanCamilo-R@users.noreply.github.com>
2021-04-16 20:42:25 -0500

    merge2

--- cliente/src/components/Cards.js
+++ cliente/src/components/Cards.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import { useState } from 'react';
-<<<<<<< HEAD
 import CarouselContainer from './CarouselContainer';
-=======
->>>>>>> a95f9f38b6860a1400c671804a4609c0d113e31b
 
 class Card extends React.Component{
       render(){
