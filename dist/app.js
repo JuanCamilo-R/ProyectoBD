483aa1021fe5004983d4305b0f8137ad78f34146
JuanCamilo-R <51805884+JuanCamilo-R@users.noreply.github.com>
2021-09-17 11:38:02 -0500

    Arreglo bug de json del backend

--- dist/app.js
+++ dist/app.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+// import ReactDOMServer from "react-dom/server";
 const modifyRequest_1 = __importDefault(require("./middlewares/modifyRequest"));
 const index_1 = require("./routes/index");
+// import App from "../cliente/src/components/App.js";
 const PORT = 4000;
 const path = require("path");
 const app = express_1.default();
@@ -14,7 +16,35 @@ app.use(express_1.default.urlencoded({ extended: false }));
 app.use(modifyRequest_1.default);
 //EndPoints
 app.use("/database", index_1.router);
+app.use(express_1.default.static(path.join(__dirname, "../", "cliente", "build")));
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../", "cliente", "build", "index.html"));
+});
+// app.use("^/$", (req, res, next) => {
+// 	fs.readFile(
+// 		path.resolve(path.join(__dirname, "../"), "build", "index.html"),
+// 		"utf-8",
+// 		(err, data) => {
+// 			if (err) {
+// 				console.log(err);
+// 				return res.status(500).send("Some error happened");
+// 			}
+// 			return res.send(
+// 				data.replace(
+// 					'<div id="root"></div>',
+// 					`<id="root"/>${ReactDOMServer.renderToString(<App />)}</div>`
+// 				)
+// 			);
+// 		}
+// 	);
+// 	// res.sendFile(
+// 	// 	path.resolve(path.join(__dirname, "../"), "cliente", "build", "index.html")
+// 	// );
+// });
+// app.use(express.static("../cliente/build"));
+// const root = require("path").join(__dirname, "../", "cliente", "build");
+// app.use(express.static(root));
 // app.get("*", (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, "cliente", "build", "index.html"));
+// 	res.sendFile("index.html", { root });
 // });
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- dist/controllers/index.controller.js
+++ dist/controllers/index.controller.js
@@ -11,6 +11,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateUser = exports.deleteUser = exports.createUser = exports.getUser = void 0;
 const pg_1 = require("pg");
+const path = require("path");
 const pool = new pg_1.Pool({
     connectionString: "postgres://rcdwvckhhgkbgc:7e78409ef8f599f449fe4290bac69d16c71b33dca005b90f0bb9f6296aeae62c@ec2-54-166-167-192.compute-1.amazonaws.com:5432/d5iiden0sct2v9",
     ssl: {
@@ -18,27 +19,54 @@ const pool = new pg_1.Pool({
     },
 });
 const array = ["Federico", "Alejandra", "Papa Robinson"];
-let result = array.reduce((acum, currentVal) => (acum += currentVal + ","), "");
-result = result.slice(0, result.length - 1);
+const convertArrayIntoPgArray = (array) => {
+    let result = array.reduce((acum, currentVal) => (acum += currentVal + ","), "");
+    return result.slice(0, result.length - 1);
+};
 const getUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { array_values, string_values, } = req.body;
-    const response = yield pool.query(`SELECT func_personas('85015', 'Messirve', 'SIUU', 'No', 1, 7, 3, 18)`);
     /*
         SELECT categoria_peliculas(cedula, id_frecuencia, id_atractivo_pelicua);
     */
     // const response = await pool.query("SELECT * FROM tabla_prueba");
-    console.log(response.rows);
-    res.send({
-        Filas: response.rows,
-    });
+    console.log(`${path.join(__dirname, "../../")}`);
+    // res.send({
+    // 	Filas: response.rows,
+    // });
 });
 exports.getUser = getUser;
 const createUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { array_values, string_values, } = req.body;
-    // const response = await pool.query(
-    // 	`INSERT INTO tabla_prueba(name, email) VALUES('${name}', '${email}')`
-    // );
-    console.log(__dirname);
+    const { cedula, nombre, apellido, hijos_si_no, genero, color, comuna, edad, ocupacion, 
+    //pelicula
+    frecuencia_pelicula, genero_favorito_pelicula, 
+    //lectura
+    adquiere_frecuencia_lectura, frecuencia_lectura, } = string_values;
+    const { 
+    //pelicula
+    comida_favorita_pelicula, atractivoPelicula, 
+    //lectura
+    idiomas_lectura, genero_favorito_lectura, } = array_values;
+    try {
+        const respuestaPersonas = yield pool.query(`SELECT func_personas('${cedula}', '${nombre}', '${apellido}', '${hijos_si_no}',${genero}, 7, ${comuna}, ${edad}, ${ocupacion})`);
+        const respuestaPelicula = yield pool.query(`SELECT func_categoria_pelicula('${cedula}',${frecuencia_pelicula}, ${genero_favorito_pelicula}, 
+		'{${convertArrayIntoPgArray(atractivoPelicula)}}', '{${convertArrayIntoPgArray(comida_favorita_pelicula)}}')`);
+        const respuestaDeporte = null;
+        const respuestaLectura = yield pool.query(`SELECT func_categoria_lectura('${cedula}', '${frecuencia_lectura}', '${adquiere_frecuencia_lectura}',
+		'{${convertArrayIntoPgArray(idiomas_lectura)}}', '{${convertArrayIntoPgArray(genero_favorito_lectura)}}')`);
+    }
+    catch (e) {
+        res.status(400).send({
+            error: "Query error, this may be because the information you have provided is not allowed",
+        });
+    }
+    finally {
+        //nothing
+    }
+    console.log({
+        array_values,
+        string_values,
+    });
     res.send({
         message: "Information has been saved!",
     });
